c6778b3179b20b4bb9cb8bbadbe7bd4e1e4a4e75
Kanagaraj <kanagarajwhb@gmail.com>
2021-11-18 22:04:48 +0530

    added grid functionality to the render images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,19 +1,13 @@
 import React from "react";
+import ImageCard from "./ImageCard";
+import "./ImageList.css";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ id, description, urls }) => {
-    return (
-      <img
-        key={id}
-        src={urls.regular}
-        alt={description}
-        width="250px"
-        height="200px"
-      />
-    );
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
